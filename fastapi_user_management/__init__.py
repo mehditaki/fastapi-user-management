9f8c3906dff5601185d374044c130a7d9f4a7613
amoo_taki <m.khoshtinat@digikala.com>
2024-05-23 15:02:54 +0330

    V1

--- fastapi_user_management/__init__.py
+++ fastapi_user_management/__init__.py
@@ -1,3 +1,5 @@
-from importlib_metadata import version
-
-__version__ = version(__package__)
+try:
+    from importlib_metadata import version
+    __version__ = version(__name__)
+except Exception:
+    __version__ = "0.1.0"
